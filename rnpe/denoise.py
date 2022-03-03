659b8263dbe50d11ba528bdd72208cd2486cada5
danielward27 <danielward27@outlook.com>
2022-03-03 17:20:50 +0000

    Update example and add plot fn

--- rnpe/denoise.py
+++ rnpe/denoise.py
@@ -2,13 +2,74 @@ import jax.numpy as jnp
 import numpyro
 import numpyro.distributions as dist
 
-def horseshoe_denoiser_model(y_obs, gaussian_noise_transfrom, tau_prior_scale=2.5e-5):
+def horseshoe_denoiser_model(y_obs, gaussian_noise_transfrom, tau_prior_scale, min_scale):
     "params and inverse transform for jax flow."
     dim = jnp.shape(y_obs)[0]
     lambdas = numpyro.sample("lambdas", dist.HalfCauchy(jnp.ones(dim)))
-    tau = numpyro.sample("tau", dist.HalfCauchy(jnp.array([tau_prior_scale])))
-    scales = lambdas*tau
+    tau = numpyro.sample("tau", dist.HalfCauchy(jnp.array(tau_prior_scale)))
+    scales = lambdas * tau + min_scale
     z = numpyro.sample("z", dist.Normal(jnp.zeros(dim), 1))  # Base dist
-    x = numpyro.deterministic(
-        "x", gaussian_noise_transfrom(z))  # Transform
+    x = numpyro.deterministic("x", gaussian_noise_transfrom(z))  # Transform
     numpyro.sample("y", dist.Normal(x, scales), obs=y_obs)
+
+
+# def spike_and_slab_denoiser_model(y_obs, gaussian_noise_transfrom, probs=None):
+#     dim = jnp.shape(y_obs)[0]
+#     if probs is None:
+#         probs = jnp.full((dim,), 0.1)
+
+#     mask = numpyro.sample("mask", dist.Bernoulli(probs))  # 1==slab==misspecified
+#     inverse_mask = mask == 0
+#     misspecified_indices = jnp.where(mask)
+
+#     z = numpyro.sample("z", dist.Normal(jnp.zeros(dim), 1))  # Base dist
+#     x = gaussian_noise_transfrom(z)
+#     x = mask * x + y_obs * inverse_mask
+#     x = numpyro.deterministic("x", x)
+
+#     numpyro.sample("y", dist.Normal(x[misspecified_indices], 5), obs=y_obs)
+
+# class MaskedNormal(dist.Distribution):
+#     support = constraints.real
+
+#     def __init__(self, normal: dist.Normal, mask: jnp.array):
+#         self.normal = normal
+#         self.mask = mask
+#         super().__init__(batch_shape=normal.batch_shape, event_shape=())
+
+#     def sample(self, key, sample_shape=()):
+#         raise NotImplementedError
+
+#     def log_prob(self, value):
+#         return (self.normal.log_prob(value)*self.mask).sum()
+
+
+# class FlowDist(dist.Distribution):
+#     support = constraints.real
+
+#     def __init__(self, flow):
+#         self.flow = flow
+#         super().__init__(batch_shape=(), event_shape=(flow.target_dim, ))
+
+#     def log_prob(self, value):
+#         value = jnp.expand_dims(value, axis=0)
+#         return self.flow.log_prob(value)[0]
+
+#     def sample(self, key, sample_shape=()):
+#         return flow.sample(key, 1)[0]  # TODO not sure if this right?
+
+
+# def spike_and_slab_denoiser_model(y_obs, gaussian_noise_transfrom, well_specified_probs):
+#     dim = jnp.shape(y_obs)[0]
+#     well_specified_mask = numpyro.sample(
+#         "well_specified_mask", dist.Bernoulli(well_specified_probs))  
+#     misspecified_mask = well_specified_mask == 0
+#     z = numpyro.sample("z", dist.Normal(jnp.zeros(dim), 1))  # Base dist. Assuming independent of mask?
+#     x = numpyro.deterministic("x", gaussian_noise_transfrom(z)*well_specified_mask + y_obs * misspecified_mask)  # shrink well specified to y_obs
+#     scales = jnp.full((dim, ), 5) # not sure about bottom two lines
+#     normal = Normal(x, scales)
+#     numpyro.sample("y", MaskedNormal(normal, well_specified_mask), obs=y_obs)  # TODO try switching this if not
+
+#     # with numpyro.plate("data", len(y_obs)) as ind:
+#     #     numpyro.sample("y", dist.Normal(x[ind], scales[ind]), obs=y_obs[ind])
+#     # pyro plate over misspecified with normal? if uniform surely slab doesn't impact log prob. Do we need the last bit?
---
+++ rnpe/plot_utils.py
@@ -0,0 +1,91 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import matplotlib
+from matplotlib.lines import Line2D
+
+
+# TODO document
+def pairplot(
+    arrays,
+    true,
+    array_point_size=0.1,
+    true_point_size=5,
+    facet_size=5,
+    names=None, # arrays and true names for legend
+    col_names=None,  # summary statistic names
+    match_size=True,
+    colors = None,
+    trim_quantile = 0.0005  # To remove big outliers that interfere with plotting
+    ):
+    if colors is None:
+        colors = list(matplotlib.colors.TABLEAU_COLORS.keys())
+        colors.remove("tab:green")  # save for true    
+        colors = colors[:len(arrays)]
+        colors.append("tab:green")
+
+    dim = len(true)
+    
+    if col_names is None:
+        col_names = ["" for i in range(dim)]
+    arrays = [np.array(a) for a in arrays]
+    true = np.array(true)
+
+    if match_size:
+        min_size = min([a.shape[0] for a in arrays])
+        arrays = [a[:min_size] for a in arrays]
+
+    if trim_quantile:
+        stacked = np.concatenate(arrays)
+        l = np.quantile(stacked, trim_quantile, axis=0),
+        u = np.quantile(stacked, 1-trim_quantile, axis=0)
+        arrays_trimmed = [
+            _trim(a, l, u) for a in arrays
+        ]
+        arrays = arrays_trimmed
+        assert len(arrays)
+
+    fig, axs = plt.subplots(dim, dim)
+    
+    for j in range(dim):
+        for i in range(dim):
+            if i < j:
+                axs[i, j].axis("off")
+            else:
+                for a, color in zip(arrays, colors[:-1]):
+                    if i==j:
+                        sns.kdeplot(a[:, i], ax = axs[i, j], color=color)
+                        axs[i, j].axvline(true[i], color=colors[-1])
+                        axs[i, j].set_ylabel("")
+                    else:
+                        axs[i, j].scatter(a[:, j], a[:, i], s=array_point_size, alpha=0.1, color=color)
+                        axs[i, j].scatter(true[j], true[i], s=true_point_size, color=colors[-1])
+            
+            if i >= j:
+                if i==(dim-1):
+                    axs[i, j].set_xlabel(col_names[j])
+                if j==0:
+                    axs[i, j].set_ylabel(col_names[i])
+
+            axs[i,j].xaxis.set_ticklabels([])
+            axs[i,j].yaxis.set_ticklabels([])
+        size = dim**0.5  # Seems reasonable default
+        fig.set_size_inches(facet_size*size, facet_size*size)
+
+    if names is not None:
+        legend_elements = _get_manual_legend(names, colors)
+        plt.figlegend(legend_elements, names, loc = "lower center", ncol=len(names))
+
+def _trim(a, l, u):
+    a = a[(a > l).all(axis=1)]
+    a = a[(a < u).all(axis=1)]
+    return a
+
+def _get_manual_legend(labels, colors):
+    legend_elements = [  # Manual legend to avoid small points in legend
+        Line2D([0], [0], marker='o', color='w', label=label,
+        markerfacecolor=color, markersize=8) for label, color in zip(labels, colors)
+        ]
+    return legend_elements
+
+    
